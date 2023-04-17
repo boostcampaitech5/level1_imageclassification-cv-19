40f019d35b14eaf9aaec969924ff4c1f588c34ce
nayoung <nynlp39@gmail.com>
2023-04-17 15:03:43 +0000

    update dataset.py: reset agebound

--- dataset.py
+++ dataset.py
@@ -114,7 +114,7 @@ class AgeLabels(int, Enum):
 
         if value < 30:
             return cls.YOUNG
-        elif value < 55: ## age bound edit 
+        elif value < 60: 
             return cls.MIDDLE
         else:
             return cls.OLD
