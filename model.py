eb33ff5b866b0cb024890f4803a87d4b682ed5a3
nayoung <nynlp39@gmail.com>
2023-04-18 06:41:24 +0000

    add FaceNet

--- model.py
+++ model.py
@@ -1,6 +1,26 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models 
+from facenet_pytorch import  MTCNN, InceptionResnetV1, fixed_image_standardization, training
+
+# Custom Model Template
+class MyModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        """
+        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
+        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
+        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
+        """
+
+    def forward(self, x):
+        """
+        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
+        2. 결과로 나온 output 을 return 해주세요
+        """
+        return x
+
 
 class BaseModel(nn.Module):
     def __init__(self, num_classes):
@@ -65,20 +85,37 @@ class MobileNet(nn.Module):
         x = self.mobilenet(x)
         return x
 
-# Custom Model Template
-class MyModel(nn.Module):
+# FaceNet
+class FaceNet(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+        
+        # For a model pretrained on VGGFace2
+        self.facenet = InceptionResnetV1(pretrained='vggface2')
+        self.facenet.classify = True
+        self.relu = nn.ReLU()
+        self.fc = nn.Linear(8631, num_classes)
 
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
-
+        '''
+        ##freeze
+        for name, param in self.facenet.named_parameters():
+            if name.split('.')[1] == 'fc' :
+                pass
+            else :
+                param.requires_grad = False
+        
+        ##freeze 확인         
+        for name, param in self.facenet.named_parameters():
+            print(name, param.requires_grad)
+        '''
+                
+                
     def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
+        
+        x = self.facenet(x)
+        x = self.relu(x)
+        return self.fc(x)
+
+# model = FaceNet(3)
+# print(model)
+
